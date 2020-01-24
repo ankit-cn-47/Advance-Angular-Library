9e8a4979e440e381b5d880c677f4227447179bb9
ankit-cn-47 <ankit.codenamefourty7@gmail.com>
2020-01-25 03:44:44 +0530

    Preprations to publish finished

--- au-input/src/app/app.component.spec.ts
+++ au-input/src/app/app.component.spec.ts
@@ -1,25 +1,48 @@
-import { TestBed, async } from '@angular/core/testing';
+import { InputRefDirective } from './lib/common/input-ref.directive';
+import { AuFaInputComponent } from './lib/au-fa-input/au-fa-input.component';
+import { TestBed, async, ComponentFixture } from '@angular/core/testing';
 
 import { AppComponent } from './app.component';
+import { DebugElement } from '@angular/core';
+import { By } from '@angular/platform-browser';
 
 describe('AppComponent', () => {
+
+    let component: AppComponent,
+        fixture: ComponentFixture<AppComponent>,
+        el: DebugElement,
+        emailField: DebugElement;
+
     beforeEach(async(() => {
         TestBed.configureTestingModule({
             declarations: [
-                AppComponent
+                AppComponent,
+                AuFaInputComponent,
+                InputRefDirective
             ],
         }).compileComponents();
     }));
 
-    it('should create the app', async(() => {
-        const fixture = TestBed.createComponent(AppComponent);
-        const app = fixture.debugElement.componentInstance;
-        expect(app).toBeTruthy();
+    beforeEach(async(() => {
+        fixture = TestBed.createComponent(AppComponent);
+        component = fixture.debugElement.componentInstance;
+        el = fixture.debugElement;
+        emailField = el.query(By.css('#email-field'));
+        fixture.detectChanges();
     }));
 
+    it('should create the app', async(() => {
+        expect(component).toBeTruthy();
+    }));
 
-
-
-
-
+    it('should create a font awesome email input', async(() => {
+        expect(emailField).toBeTruthy();
+    }));
+    it('should contain correct email icon', async(() => {
+        console.log(emailField.nativeElement.outerHTML);
+        expect(emailField.query(By.css('i.icon.fa.fa-envelope'))).toBeTruthy();
+    }));
+    it('should contain a input field', async(() => {
+        expect(emailField.query(By.css('input.test-class'))).toBeTruthy();
+    }));
 });
---
+++ au-input/src/app/lib/au-input.module.ts
@@ -0,0 +1,23 @@
+import { InputRefDirective } from './common/input-ref.directive';
+import { AuFaInputComponent } from './au-fa-input/au-fa-input.component';
+import { NgModule } from '@angular/core';
+import { CommonModule } from '@angular/common';
+
+
+@NgModule({
+    imports: [
+        CommonModule
+    ],
+    declarations: [
+        AuFaInputComponent,
+        InputRefDirective
+    ],
+    exports: [
+        AuFaInputComponent,
+        InputRefDirective
+    ]
+})
+
+export class AuInputModule {
+
+}
